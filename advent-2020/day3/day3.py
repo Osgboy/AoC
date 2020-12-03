d47fa256081aeeb84f56767a1a8a8a17c7c54037
Osgboy <32489148+Osgboy@users.noreply.github.com>
2020-12-03 00:32:56 -0500

    Day 3

--- advent-2020/day3/day3.py
+++ advent-2020/day3/day3.py
@@ -1,1 +1,18 @@
 #https://adventofcode.com/2020/day/3
+with open("day3.in", "r") as fin:
+    slope = list(fin.readlines())
+slope = [x.strip() for x in slope]
+print(slope)
+
+x=0
+y=0
+trees = 0
+while y < len(slope):
+    x += 3
+    y += 1
+    #print(x,y)
+    if slope[y][x%len(slope[0])] == "#":
+        trees += 1
+    print(trees)
+
+print(trees)
--- advent-2020/day3/day3part2.py
+++ advent-2020/day3/day3part2.py
@@ -1,1 +1,22 @@
 #https://adventofcode.com/2020/day/3
+with open("day3.in", "r") as fin:
+    slope = list(fin.readlines())
+slope = [x.strip() for x in slope]
+print(slope)
+
+out = 1
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+for s in slopes:
+    x=0
+    y=0
+    trees = 0
+    while y < len(slope)-1:
+        x += s[0]
+        y += s[1]
+        #print(x,y)
+        if slope[y][x%len(slope[0])] == "#":
+            trees += 1
+        print(trees)
+    out *= trees
+
+print(out)
