336a18a2e4a733c3edc66597518f4dc87d54066d
Osgboy <32489148+Osgboy@users.noreply.github.com>
2020-12-05 01:09:11 -0500

    Day 5 improved

--- advent-2020/day5/day5.py
+++ advent-2020/day5/day5.py
@@ -4,33 +4,16 @@ import math
 with open("day5.in", "r") as fin:
     seats = list(fin.readlines())
 
-
-def div(half, c):
-    low = half[0]
-    high = half[1]
-    if c == 'F' or c == 'L':
-        return low, math.floor(low + ((high - low) / 2))
-    else:
-        return math.ceil(low + ((high - low) / 2)), high
-
-
-a = div([0, 127], 'F')
-print(a)
-b = div(a, 'B')
-print(b)
-
 out = 0
 for seat in seats:
-    Rrange = (0, 127)
-    for n in seat[:7]:
-        Rrange = div(Rrange, n)
-        print(Rrange)
-    Crange = (0, 7)
-    for n in seat[7:]:
-        Crange = div(Crange, n)
-        print(Crange)
-    row = Rrange[0]
-    col = Crange[0]
+    row = 0
+    for r in range(7):
+        if seat[r] == 'B':
+            row += 2 ** (6 - r)
+    col = 0
+    for c in range(7, 10):
+        if seat[c] == 'R':
+            col += 2 ** (9 - c)
     ID = 8 * row + col
     if ID > out:
         out = ID
