8a20e61a63787590414944e4da83e3a80a139e97
Osgboy <32489148+Osgboy@users.noreply.github.com>
2021-08-19 12:03:03 -0400

    2019 Day 9

---
+++ advent-2019/day09/day9.py
@@ -0,0 +1,119 @@
+# https://adventofcode.com/2019/day/9
+from collections import defaultdict
+
+with open('day9.in', 'r') as fin:
+    raw = [int(a) for a in fin.readline().split(",")]
+    print(raw)
+    original = defaultdict(int)
+    for k, v in enumerate(raw):
+        original[k] += v
+    print(original)
+
+def first(instruct):
+    out = [int(instruct[-2:])]
+    for x in range(-3, -6, -1):
+        try:
+            out.append(int(instruct[x]))
+        except IndexError:
+            out.append(0)
+    return out
+
+
+class Computer:
+    def __init__(self, compinput):
+        self.compInput = compinput
+        self.intcode = original.copy()
+        self.x = 0
+        self.output = None
+        self.relBase = 0
+
+    def paramMode(self, mode, value):
+        if mode == 0:  # position
+            return self.intcode[value]
+        elif mode == 1:  # immediate
+            return value
+        elif mode == 2:  # relative
+            return self.intcode[value] + self.relBase
+
+    def one(self, a, b, c):
+        self.intcode[c] = self.intcode[a] + self.intcode[b]
+        self.x += 4
+
+    def two(self, a, b, c):
+        self.intcode[c] = self.intcode[a] * self.intcode[b]
+        self.x += 4
+
+    def three(self, compInput, a):
+        self.intcode[a] = compInput
+        self.x += 2
+
+    def four(self, a):
+        self.x += 2
+        return self.intcode[a]
+
+    def five(self, a, b):
+        if self.intcode[a]:
+            self.x = self.intcode[b]
+        else:
+            self.x += 3
+
+    def six(self, a, b):
+        if self.intcode[a]:
+            self.x += 3
+        else:
+            self.x = self.intcode[b]
+
+    def seven(self, a, b, c):
+        if self.intcode[a] < self.intcode[b]:
+            self.intcode[c] = 1
+        else:
+            self.intcode[c] = 0
+        self.x += 4
+
+    def eight(self, a, b, c):
+        if self.intcode[a] == self.intcode[b]:
+            self.intcode[c] = 1
+        else:
+            self.intcode[c] = 0
+        self.x += 4
+
+    def nine(self, a):
+        self.relBase += self.intcode[a]
+        self.x += 2
+
+    def compute(self):
+        while self.x <= len(self.intcode):
+            op, modeA, modeB, modeC = first(str(self.intcode[self.x]))
+            try:
+                a = self.paramMode(modeA, self.x + 1)
+                b = self.paramMode(modeB, self.x + 2)
+                c = self.paramMode(modeC, self.x + 3)
+            except IndexError:
+                pass
+            if op == 1:
+                self.one(a, b, c)
+            elif op == 2:
+                self.two(a, b, c)
+            elif op == 3:
+                self.three(self.compInput, a)
+            elif op == 4:
+                self.output = self.four(a)
+                print('output', self.output)
+            elif op == 5:
+                self.five(a, b)
+            elif op == 6:
+                self.six(a, b)
+            elif op == 7:
+                self.seven(a, b, c)
+            elif op == 8:
+                self.eight(a, b, c)
+            elif op == 9:
+                self.nine(a)
+            elif op == 99:
+                print('Done')
+                break
+
+part1 = Computer(1)
+part2 = Computer(2)
+part1.compute()
+part2.compute()
