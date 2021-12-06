426055ae8165f58a25cc2396cf4c4d2a28399bfb
Osgboy <32489148+Osgboy@users.noreply.github.com>
2021-12-06 11:53:34 -0500

    2021 Day 6

---
+++ advent-2021/day06/day06part2.py
@@ -0,0 +1,14 @@
+# https://adventofcode.com/2021/day/6
+from collections import Counter
+
+with open('day06.in', 'r') as fin:
+    fish = Counter([int(x) for x in fin.readline().split(',')])
+
+for day in range(256):
+    yesterday = fish[0]
+    for x in range(6):
+        fish[x] = fish[x+1]
+    fish[6] = yesterday + fish[7]
+    fish[7] = fish[8]
+    fish[8] = yesterday
+    print(fish.total())
