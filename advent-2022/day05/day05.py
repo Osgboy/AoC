9cba138364c3b6cb96f4f3360b0286558096ad34
Oliver <olivergu100@gmail.com>
2022-12-05 16:42:55 -0500

    2022 Day 5

---
+++ advent-2022/day05/day05.py
@@ -0,0 +1,27 @@
+# https://adventofcode.com/2022/day/5
+
+with open("day05.in", "r") as fin:
+    raw = fin.readlines()
+
+div = raw.index('\n')
+crates = raw[:div - 1]
+nCrates = int(raw[div - 1].strip()[-1])
+proc = raw[div + 1:]
+stacks = {x: [] for x in range(1, nCrates + 1)}
+
+for n in reversed(crates):
+    for c in range(1, len(n), 4):
+        if n[c] != ' ':
+            stacks[int((c - 1) / 4) + 1].append(n[c])
+
+for p in proc:
+    [n, a, b] = [int(s) for s in p.split() if s.isdigit()]
+    for x in range(n):
+        stacks[b].append(stacks[a].pop())
+
+print(stacks)
+last = []
+for s in range(1, nCrates + 1):
+    last.append(stacks[s][-1])
+
+print(''.join(last))
